41d6811fcc24da886ca5c6836e8819cc5ce86b93
1nk_0 <dksud27@naver.com>
2021-03-18 22:47:01 +0900

    ADD reactHooks
    * ADD useInput Hooks

---
+++ reactHooks/useinput.js
@@ -0,0 +1,20 @@
+//조건에 맞는 input 입력만 받기
+const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
+      const {
+        target: { value }
+      } = event;
+  
+      let willUpdate = true;
+  
+      if (typeof validator === "function") {
+        willUpdate = validator(value);
+      }
+  
+      if (willUpdate) {
+        setValue(value);
+      }
+    };
+    return { value, onChange };
+  };
