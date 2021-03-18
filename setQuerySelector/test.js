b9b540fb63399b845713c67d717a89d376d43679
1nk_0 <dksud27@naver.com>
2021-03-18 12:28:34 +0900

    FIX setQuerySelector
    * FIX return result location

--- setQuerySelector/test.js
+++ setQuerySelector/test.js
@@ -1,25 +1,19 @@
-const visited = [];
-const query = (select, root = document) => {
-  console.log("*****************************");
-  console.log("위의 root:",root);
-  // console.log("select:",select);
+
+const query = (select, root = document,visited=[]) => {
   if (root === null) return;
   if (root.className === select) {
-    console.log("마지막답:",root);
     return root;
   } else {
     let result;
-    root.childNodes.forEach((node,i) => {
+    root.childNodes.forEach((node) => {
       if(result) return;
       if (!visited.includes(node)) {
         visited.push(node);
         root = node;
-        console.log(i,"안의 바뀐 root(node):",root);
-
         result = query(select, root);
       }
-      else return result;
     });
+    return result;
   }
 };
 
